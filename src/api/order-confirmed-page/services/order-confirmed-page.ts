cec65e03336a8a645516c59a47c571fc800688df
mgia007 <georgia.mihalca@mtdtechnology.net>
2026-09-04 13:16:22 +0300

    order confirmation page and empty cart text

---
+++ src/api/order-confirmed-page/services/order-confirmed-page.ts
@@ -0,0 +1,7 @@
+/**
+ * order-confirmed-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::order-confirmed-page.order-confirmed-page');
